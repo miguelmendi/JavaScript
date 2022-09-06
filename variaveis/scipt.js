0f9b16944658fc86849a3dfead6fea6b44df720a
miguelmendi <miguelmds925@gmail.com>
2022-09-06 17:20:35 -0300

    Aprendendo sobre varíaveis no JavaScript

---
+++ variaveis/scipt.js
@@ -0,0 +1,51 @@
+var nome = "andre";
+var idade = "25";
+var possuifaculdade = true;
+
+console.log(nome, idade, possuifaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Mendes",
+  cidade = "Brasília";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaodefine);
+
+var comida;
+console.log(comida);
+comida = "Pizza";
+console.log(comida);
+
+//let time = "Vasco";
+//time = "Flamengo";
+
+//console.log(time);
+
+// Declarar uma variável com o seu nome
+var Nome = "18";
+
+// Declarar uma variável com a sua idade
+var Idade = "25";
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var ComidaFavorita;
+
+// Atribuir valor a sua comida favorita
+ComidaFavorita = "Lasanha";
+
+// Declarar 5 variáveis diferentes sem valores
+var semvalor;
+var semvalor2;
+var semvalor3;
+var semvalor4;
+var semvalor5;
