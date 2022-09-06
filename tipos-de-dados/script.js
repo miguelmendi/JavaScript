f5518c47ba5a0d22574630fcbde9338f0bfbe068
miguelmendi <miguelmds925@gmail.com>
2022-09-06 19:05:57 -0300

    Aprendendo os tipos de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,55 @@
+var nome = "Miguel";
+var idade = 28;
+var símbolo = Symbol();
+console.log(typeof símbolo);
+
+var nome = "Miguel";
+var sobrenome = "Mendes";
+var nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+//var gols = 1000;
+//var frase = "Romário fez " + gols + " gols";
+//console.log(typeof frase);
+
+var ano = "2018";
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = "teste";
+var frase1 = 'Esse é o "melhor" jogo';
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols * 2}`;
+
+console.log(frase2);
+
+// Declare uma variável contendo uma string
+
+var conteudo = "Isso é uma String";
+
+// Declare uma variável contendo um número dentro de uma string
+
+var ano = "2022";
+
+// Declare uma variável com a sua idade
+
+var minhaIdade = 18;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+var MeuNome = "Miguel ";
+var MeuSobrenome = "Mendes";
+var MeuNomeCompleto = `${MeuNome} ${MeuSobrenome}`;
+console.log(MeuNomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var PalavraInicial = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+
+console.log(typeof MeuNomeCompleto);
