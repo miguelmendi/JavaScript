2871386f868df134df31e5d64711bdd525929f3e
miguelmendi <miguelmds925@gmail.com>
2022-09-07 18:51:34 -0300

    Aprendendo os numeros e operadores

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,24 @@
+var idade = 2.5;
+
+console.log(idade);
+
+var total = 10 + 5 + 10;
+var divisao = 200 / 5;
+var modulo = 31545688872 % 3;
+
+console.log(divisao);
+
+var testeNaN = "Isso é 100" / 2;
+console.log(testeNaN);
+
+var soma1 = 10 + 10 + 20 + (30 * 4) / 2 + 10;
+console.log(soma1);
+
+var x = 5;
+console.log(x++);
+console.log(x);
+
+var idade = "28";
+var somaIdade = 5;
+
+console.log(+idade + somaIdade);
