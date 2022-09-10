c5be28595b8326d0fdcc098deb7d7034a39dba1c
miguelmendi <miguelmds925@gmail.com>
2022-09-10 18:12:02 -0300

    Fazendo o exercicio de números e operadores

--- numeros-e-operadores/script.js
+++ numeros-e-operadores/script.js
@@ -22,3 +22,35 @@ var idade = "28";
 var somaIdade = 5;
 
 console.log(+idade + somaIdade);
+
+// Qual o resultado da seguinte expressão?
+var total1 = 10 + (5 * 2) / 2 + 20;
+
+resultado = 35;
+
+// Crie duas expressões que retornem NaN
+
+var venda = "vendi 10" / 5;
+console.log(venda);
+
+var numero = 60;
+var unidade = "ml";
+var mililitros = numero + unidade;
+var litros = mililitros * 1000;
+console.log(litros);
+
+// Somar a string '200' com o número 50 e retornar 250
+
+var total = +"200" + 50;
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+var y = 5;
+++y;
+console.log(y);
+
+// Como dividir o peso por 2?
+var numero2 = +"80" / 2;
+var unidade2 = "kg";
+var peso = numero2 + unidade2; // '80kg'
+console.log(peso);
