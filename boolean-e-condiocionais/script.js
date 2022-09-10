d1464f8495719661c39cea2d9adc4e53f631afda
miguelmendi <miguelmds925@gmail.com>
2022-09-10 19:53:25 -0300

    Aprendendo sobre condicionais e boolean

---
+++ boolean-e-condiocionais/script.js
@@ -0,0 +1,120 @@
+var possuiGraduacao = false;
+var possuiDoutorado = true;
+
+if (possuiGraduacao) {
+  console.log("É verdadeiro");
+} else if (possuiDoutorado) {
+  console.log("Possui Doutorado");
+} else {
+  console.log("Não possui nada");
+}
+
+var nome = "André";
+
+if (nome) {
+  console.log(nome);
+} else {
+  console.log("Nome não existe");
+}
+
+if (!possuiGraduacao) {
+  console.log("Não possui graduação");
+}
+
+var x = 10;
+
+console.log(x === 10);
+
+var x = "Gatos";
+
+console.log(x !== "Gato");
+
+if (5 - 5 && 5 + 5) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+var condicional = 5 - 10 && 5 + 5;
+if (condicional) {
+  console.log("Verdadeiro", condicional);
+} else {
+  console.log("Falso");
+}
+
+var condicional2 = 5 - 5 || 5 + 5 || 10 - 2;
+console.log(condicional2);
+
+var corFavorita = "Verde";
+
+switch (corFavorita) {
+  case "Azul":
+    console.log("Olhe para o céu");
+    break;
+  case "Amarelo":
+    console.log("Olhe para o sol.");
+    break;
+  case "Verde":
+    console.log("Olhe para a floresta");
+    break;
+  default:
+    console.log("Feche os olhos");
+}
+
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+
+var minhaIdade = 22;
+var idadeParente = 25;
+
+if (minhaIdade > idadeParente) {
+  console.log("É maior");
+} else if (minhaIdade === idadeParente) {
+  console.log("É igual");
+} else {
+  console.log("É menor");
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = 5 - 2 && 5 - " " && 5 - 2;
+
+//3
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = "Andre"; //true
+var idade = 28; //true
+var possuiDoutorado = false; //false
+var empregoFuturo; // false
+var dinheiroNaConta = 0; //false
+
+console.log(
+  !!nome,
+  !!idade,
+  !!possuiDoutorado,
+  !!empregoFuturo,
+  !!dinheiroNaConta
+);
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if (brasil > china) {
+  console.log("Brasil tem mais habitantes");
+} else {
+  console.log("Brasil tem menos habitantes");
+}
+
+// O que irá aparecer no console?
+if ("Gato" === "gato" && 5 > 2) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+// O que irá aparecer no console?
+if ("Gato" === "gato" || 5 > 2) {
+  console.log("Gato" && "Cão");
+} else {
+  console.log("Falso");
+}
